876af626633f9f67dc31af72d348f5fd3ba1f580
unknown <ragnhildurg19@ru.is>
2019-09-05 12:02:16 +0000

    æksdjfækajsdf

---
+++ sequence.py
@@ -0,0 +1,28 @@
+#Design an algorithm that generates the first n numbers 
+# in the following sequence:; 1, 2, 3, 6, 11, 20, 37, ___, ___, ___, …
+
+n = int(input("Enter the length of the sequence: ")) # Do not change this line
+i1 = 0
+i2 = 0
+i3 = 0
+sum_int = 0
+count = 1
+while count <= n:
+    if count == 1:
+        i1 = count
+    elif count == 2:
+        i2 = count
+    elif count ==3:
+        i3 = count
+    else: 
+        sum_int = i1 + i2 + i3
+        i1 = i2
+        i2 = i3
+        i3 = sum_int
+    count +=1
+    print(sum_int)
+    count +=1
+    i1 = i2 + i1
+    i2 = i3 + i2
+    i3 = sum_int
+
