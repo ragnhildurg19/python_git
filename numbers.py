844b8b98d8e0dd2d8d9d004f0f9d27c32b7d3113
unknown <ragnhildurg19@ru.is>
2019-09-07 00:02:32 +0000

    Project 3: Special numbers

--- numbers.py
+++ numbers.py
@@ -7,6 +7,5 @@ for number in range(10, 100):
     sum_int = first_int + secound_int 
 #If number and the sum are equl, print number
     if number == sum_int**2:
-        print(number,"=", first_int, "+", secound_int, "=", sum_int)
         square = sum_int**2
-        print(sum_int, "^2 =",square)
+        print(number,"=", first_int, "+", secound_int, "=", sum_int,"^2 =",square)
