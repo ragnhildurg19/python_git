28b4a9e9484f71113619bfad537f0a3eda1a440b
unknown <ragnhildurg19@ru.is>
2019-09-30 23:01:26 +0000

    Get ekki meir

--- population.py
+++ population.py
@@ -1,28 +1,8 @@
-# DESCRIPTION
-# In this project, you develop a program, population.py, that reads population figures for US states 
-# for a particular year and prints out the states that have the minimum and maximum population for the given year.
-# The first row is a header row.  
-# The population figures for each state are shown in three columns, for years 1990, 1980 and 1970.  
-# Notice that the header years and the number of population columns (>= 1), 
-# and of course the population figures, could be different in another input file.  
-# The population figures only contain digits (no commas or periods). 
-# Note also that the name of a state can possibly consist of two words (but not more).
-# The input to the program is a name of a file in the above format 
-# and the year the user is interested in. For example, 
-# if the user enters the year 1980 she is only interested in data from that particular column.  
-# The output of the program is information about the states that had the minimum and maximum population 
-# for the given year.  Hint: The min() and max() funciton or sorted() function might come in handy!
-# Note that the minimum and maximum are tuples.  
-# This suggest that you could indeed use list of tuples in your implementation.
-
-# Your program should print out an error message and quit if the input file is not found 
-# (you should however not call the quit() function).  
-# Moreover, it should ask the user, repeatedly, to input a valid year if an invalid year is given:
-
-# 2. Biðja um ártal. 
-# 3. Prenta út max og min (borg og fjölda)
-
 import string
+def InputFromUser():
+    '''Takes input from user'''
+    file_name = input("Enter name of file: ")
+    return file_name
 
 def OpenFile(file_name):
     '''Opnar skrá, ef skrá er ekki til þá stoppar forritið með skilaboðum. '''
@@ -34,15 +14,19 @@ def OpenFile(file_name):
         print("Filename "+file_name+" not found!")
     return False
 
+def MakeList(file_object):
+    '''Make a list of lines in file, takes out whitespace (exept the first) and taps'''
+    file_list = [line.replace(" ", "", 1).strip() for line in file_object]
+    return file_list
 
+def main():
 # Fáum inn skrárnafn.
-file_name = input("Enter name of file: ")
-file_object = OpenFile(file_name)
-
-if file_object != False:
-    file_list = []
-    for line in file_object:
-        file_list = [line]
-        print(file_list)
-
+    file_name = InputFromUser()
+    file_object = OpenFile(file_name)
+    file_list = MakeList(file_object)
+    
     file_object.close()
+
+#call the main function to run the program
+main() 
+
